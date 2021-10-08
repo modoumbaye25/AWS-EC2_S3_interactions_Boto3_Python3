c5415594cd6d3091bb4ceaa1b2bd535f54490e7b
Modou Mbaye <modoumbaye25@gmail.com>
2021-10-07 19:35:13 -0700

    Update test_ec2_and_s3_interactions.py

--- test/test_ec2_and_s3_interactions.py
+++ test/test_ec2_and_s3_interactions.py
@@ -3,7 +3,7 @@
 from unittest import TestCase
 from ec2_and_s3_boto3_interaction import instances
 
-yoo = {'Reservations': [{'Instances': [{'ImageId': 'ami-123',
+sample = {'Reservations': [{'Instances': [{'ImageId': 'ami-123',
                                         'InstanceId': 'i-123',
                                         'InstanceType': 't2.micro',
                                         'State': {'Code': 0, 'Name': 'pending'}},
@@ -24,10 +24,10 @@ class Test(TestCase):
 
     # should pass
     def test_instances(self):
-        instances_list = instances(yoo)
+        instances_list = instances(sample)
         self.assertTrue('i-123' in instances_list[1])
 
     # should fail
     def test_instances(self):
-        instances_list = instances(yoo)
+        instances_list = instances(sample)
         self.assertFalse('i-321' in instances_list[1])
